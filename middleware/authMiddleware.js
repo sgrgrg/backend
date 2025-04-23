0f597080702c3a4d6c85fd0f834c05cc64a40b12
sagarr <sgrgrg34@gmail.com>
2025-04-23 14:53:49 +0545

    Add all project files

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
+  console.log("Authorization header received:", authHeader);
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
@@ -9,6 +10,7 @@ const authMiddleware = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded token:", decoded);
     req.user = { id: decoded.id };
     next();
   } catch (err) {
